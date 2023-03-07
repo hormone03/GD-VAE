38f8ed49e226d339b1530afa9d9ad3f529cab4b3
hormone03 <hormone03@yahoo.com>
2023-03-07 15:26:44 -0500

    ProdLDA added

---
+++ imageScripts/podLDA.py
@@ -0,0 +1,264 @@
+#from __future__ import print_function
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+#from LogisticRegression_VAE import LogisticRegression
+from tqdm import tqdm
+
+from utils import train_classifier, evaluate_classifier
+from utils import return_model_accurary
+from Visualisations import t_SNE, describe_statistic_per_label, show_confusion_matrix
+
+
+parser = argparse.ArgumentParser(description='Dir-VAE MNIST Example')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)') #epoch was 10
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=10, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=2, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--category', type=int, default=10, metavar='K',
+                    help='the number of categories in the dataset')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)  
+
+device = torch.device("cuda" if args.cuda else "cpu")
+
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+    batch_size=args.batch_size, shuffle=False, **kwargs)
+
+ngf = 64
+ndf = 64
+nc = 1
+
+def prior(K, alpha):
+    """
+    Prior for the model.
+    :K: number of categories
+    :alpha: Hyper param of Dir
+    :return: mean and variance tensors
+    """
+    # ラプラス近似で正規分布に近似
+    # Approximate to normal distribution using Laplace approximation
+    a = torch.Tensor(1, K).float().fill_(alpha)
+    mean = a.log().t() - a.log().mean(1)
+    var = ((1 - 2.0 / K) * a.reciprocal()).t() + (1.0 / K ** 2) * a.reciprocal().sum(1)
+    return mean.t(), var.t() # Parameters of prior distribution after approximation
+
+class Dir_VAE(nn.Module):
+    def __init__(self):
+        super(Dir_VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            # input is (nc) x 28 x 28
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 14 x 14
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 7 x 7
+            nn.Conv2d(ndf * 2, ndf * 4, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 4 x 4
+            nn.Conv2d(ndf * 4, 1024, 4, 1, 0, bias=False),
+            # nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            # nn.Sigmoid()
+        )
+
+        self.decoder = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(     1024, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2,     nc, 4, 2, 1, bias=False),
+            # nn.BatchNorm2d(ngf),
+            # nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            # nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            # nn.Tanh()
+            nn.Sigmoid()
+            # state size. (nc) x 64 x 64
+        )
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc21 = nn.Linear(512, args.category)
+        self.fc22 = nn.Linear(512, args.category)
+
+        self.fc3 = nn.Linear(args.category, 512)
+        self.fc4 = nn.Linear(512, 1024)
+
+        self.lrelu = nn.LeakyReLU()
+        self.relu = nn.ReLU()
+
+        # Dir prior
+        self.prior_mean, self.prior_var = map(nn.Parameter, prior(args.category, 0.3)) # 0.3 is a hyper param of Dirichlet distribution
+        self.prior_logvar = nn.Parameter(self.prior_var.log())
+        self.prior_mean.requires_grad = False
+        self.prior_var.requires_grad = False
+        self.prior_logvar.requires_grad = False
+
+
+    def encode(self, x):
+        conv = self.encoder(x);
+        h1 = self.fc1(conv.view(-1, 1024))
+        return self.fc21(h1), self.fc22(h1)
+
+    def decode(self, gauss_z):
+        dir_z = F.softmax(gauss_z,dim=1) 
+        # This variable (z) can be treated as a variable that follows a Dirichlet distribution (a variable that can be interpreted as a probability that the sum is 1)
+        # Use the Softmax function to satisfy the simplex constraint
+        # シンプレックス制約を満たすようにソフトマックス関数を使用
+        h3 = self.relu(self.fc3(dir_z))
+        deconv_input = self.fc4(h3)
+        deconv_input = deconv_input.view(-1,1024,1,1)
+        return self.decoder(deconv_input)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        gauss_z = self.reparameterize(mu, logvar) 
+        # gause_z is a variable that follows a multivariate normal distribution
+        # Inputting gause_z into softmax func yields a random variable that follows a Dirichlet distribution (Softmax func are used in decoder)
+        dir_z = F.softmax(gauss_z,dim=1) # This variable follows a Dirichlet distribution
+        return self.decode(dir_z), mu, logvar, gauss_z, dir_z
+
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    def loss_function(self, recon_x, x, mu, logvar, K):
+        beta = 1.0
+        BCE = F.binary_cross_entropy(recon_x.view(-1, 784), x.view(-1, 784), reduction='sum')
+        # ディリクレ事前分布と変分事後分布とのKLを計算
+        # Calculating KL with Dirichlet prior and variational posterior distributions
+        # Original paper:"Autoencodeing variational inference for topic model"-https://arxiv.org/pdf/1703.01488
+        prior_mean = self.prior_mean.expand_as(mu)
+        prior_var = self.prior_var.expand_as(logvar)
+        prior_logvar = self.prior_logvar.expand_as(logvar)
+        var_division = logvar.exp() / prior_var # Σ_0 / Σ_1
+        diff = mu - prior_mean # μ_１ - μ_0
+        diff_term = diff *diff / prior_var # (μ_1 - μ_0)(μ_1 - μ_0)/Σ_1
+        logvar_division = prior_logvar - logvar # log|Σ_1| - log|Σ_0| = log(|Σ_1|/|Σ_2|)
+        # KL
+        KLD = 0.5 * ((var_division + diff_term + logvar_division).sum(1) - K)        
+        return BCE + KLD
+
+
+model = Dir_VAE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+Rg_model = LogisticRegression(random_state=16, solver='lbfgs', max_iter=3000)
+
+def train(epoch, data, train_loss):
+    data = data.to(device)
+    optimizer.zero_grad()
+    recon_batch, mu, logvar, gauss_z, dir_z = model(data)
+    #logistic_z = gauss_z.cpu().detach()
+    #Rg_model = Rg_model.fit(data.view(-1, 784).cpu(), Y.cpu())
+    #RegAccuracy = Rg_model.score(recon_batch.view(-1, 784).detach().cpu(), Y.cpu())
+    loss = model.loss_function(recon_batch, data, mu, logvar, args.category)
+    loss = loss.mean()
+    loss.backward()
+    train_loss += loss.item()
+    optimizer.step()
+    if batch_idx % args.log_interval == 0:
+        #print(f"gause_z:{gauss_z[0]}") # Variables following a normal distribution after Laplace approximation
+        #print(f"dir_z:{dir_z[0]},SUM:{torch.sum(dir_z[0])}") # Variables that follow a Dirichlet distribution. This is obtained by entering gauss_z into the softmax function
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader),
+            loss.item() / len(data)))
+        #print('Logistic model accuracy = {:.4f}'.format(RegAccuracy))
+
+    #print('====> Epoch: {} Average loss: {:.4f}'.format(
+         # epoch, train_loss / len(train_loader.dataset)))
+    #print('====> Average logistic model accuracy = {}'.format(RegAccuracy))
+    return recon_batch, train_loss
+
+def trainLG(Rg_model, recon_batch, data):
+    #logistic_z = gauss_z.cpu().detach()
+    Rg_model = Rg_model.fit(data.view(-1, 784).cpu(), Y.cpu())
+    RegAccuracy = Rg_model.score(recon_batch.view(-1, 784).detach().cpu(), Y.cpu())
+    if batch_idx % args.log_interval == 0:
+        print('Logistic model accuracy = {:.4f}'.format(RegAccuracy))
+    return Rg_model
+
+    
+
+def test(epoch, data, test_loss):
+    model.eval()
+    with torch.no_grad():
+        data = data.to(device)
+        recon_batch_test, mu, logvar, gauss_z, dir_z = model(data)
+        loss = model.loss_function(recon_batch_test, data, mu, logvar, args.category)
+        test_loss += loss.mean()
+        test_loss.item()
+        if i == 0:
+            n = min(data.size(0), 18)
+            comparison = torch.cat([data[:n],
+                                  recon_batch_test.view(args.batch_size, 1, 28, 28)[:n]])
+            save_image(comparison.cpu(),
+                     'image/recon_' + str(epoch) + '.png', nrow=n)
+    return recon_batch_test, test_loss
+
+   
+def testLG(Rg_model, recon_batch_test):
+    model.eval()
+    with torch.no_grad():
+        RegAccuracy = Rg_model.score(recon_batch_test.view(-1, 784).detach().cpu(), label.cpu())
+    return RegAccuracy
+    
+
+if __name__ == "__main__":
+    # 学習(Train)
+    for epoch in range(1, args.epochs + 1):
+        train_loss = 0
+        for batch_idx, (data, Y) in enumerate(train_loader):
+            recon_batch, train_loss = train(epoch, data, train_loss)
+            Rg_model = trainLG(Rg_model, recon_batch, data)
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+        test_loss = 0
+        for i, (data, label) in enumerate(test_loader):
+            recon_batch_test, test_loss = test(epoch, data, test_loss)
+            RegAccuracy = testLG(Rg_model, recon_batch_test)
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+        print('====> Test set for Logistic Regression accuracy: {:.4f}'.format(RegAccuracy))
+        with torch.no_grad():
+            sample = torch.randn(64, args.category).to(device)
+            sample = model.decode(sample).cpu()
+            save_image(sample.view(64, 1, 28, 28),'image/sample_' + str(epoch) + '.png')
+            
+    show_confusion_matrix(test_loader, model, Rg_model, use_cuda=True)
